ecf0223dd4ec94abe5ff90a31bdcd369f1640659
oleksandralaskaj <alexandravalko@gmail.com>
2024-08-05 17:57:28 +0200

    rename page title

--- src/index.php
+++ src/index.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['message'])) {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>Oscar data upload</title>
+    <title>Nahrání souborů</title>
 </head>
 <body>
 <div class="container-md border p-5 m-5 rounded">
